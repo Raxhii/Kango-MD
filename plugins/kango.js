7191d683998064d1e9e2e101ec17939c6315f557
OfficialHectorManuel <143950168+OfficialHectorManuel@users.noreply.github.com>
2024-12-04 19:27:08 +0000

    Update and rename miku.js to kango.js

--- plugins/kango.js
+++ plugins/kango.js
@@ -2,8 +2,8 @@ const config = require('../config')
 const {cmd , commands} = require('../command')
 
 cmd({
-    pattern: "miku",
-    desc: "Check bot online or no.",
+    pattern: "Kango",
+    desc: "Check bot online or not.",
     category: "main",
     react: "👋",
     filename: __filename
@@ -11,21 +11,22 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-let des = `👋 𝙷𝚎𝚕𝚕𝚘 ${pushname} 𝙸'𝚖 𝚊𝚕𝚒𝚟𝚎 𝚗𝚘𝚠
+let des = `👋 𝙷𝚎𝚕𝚕𝚘 ${pushname} 𝙸'𝚖 not dead yet
 
-*Im MIKU MD Whatsapp Bot Create By Malvin King 🍂✨*
+*I'm Kango MD Whatsapp Bot Created By Hector Manuel 🍂✨*
 
 | *Version*: 1.0.0
 | *Memory*: 38.09MB/7930MB
-| *Owner*: Malvin King
+| *Owner*: Hector Manuel
 
-I am Miku md whatsapp bot. How can I help you.
+I am Kango md whatsapp bot. How can I help you.
 To get the menu, type as .menu . If you need to know something about the bot,
-type as owner and direct the question to me. Good day.
+type as owner and direct the question to me.
+You can also recommend me to you sis 😄..Good day.
 
-*°᭄🇿🇼™️ 𝙼𝙸𝙺𝚄 𝙼𝙳*
+*°🇬🇭™️ 𝐊𝐚𝐧𝐠𝐨-𝐌𝐃*
 
- > © ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍᴀʟᴠɪɴ ᴋɪɴɢ`
+ > © ▂▃▅▇█▓▒░𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝙱𝚢 𝙷𝚎𝚌𝚝𝚘𝚛 𝙼𝚊𝚗𝚞𝚎𝚕░▒▓█▇▅▃▂`
 return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: des},{quoted: mek})
 }catch(e){
 console.log(e)
