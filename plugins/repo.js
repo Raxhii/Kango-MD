7b2268c71a909dd0f966b54667a96d1d40ebcdb4
OfficialHectorManuel <143950168+OfficialHectorManuel@users.noreply.github.com>
2024-12-04 16:25:43 +0000

    Update repo.js

--- plugins/repo.js
+++ plugins/repo.js
@@ -15,15 +15,15 @@ let dec = `*👋 Hello ${pushname}*
           
 📍𝖱𝖾𝗉𝗈 𝖫𝗂𝗇𝗄 ❤️‍🔥👇
 
-👨‍💻◦ https://github.com/kingmalvn/MIKU-MD 
+👨‍💻◦ https://github.com/
 
 📍𝖯𝗅𝖾𝖺𝗌𝖾 𝖲𝗎𝖻𝗌𝖼𝗋𝗂𝖻𝖾 𝖬𝗒 𝖸𝗈𝗎𝗍𝗎𝖻𝖾 𝖢𝗁𝖺𝗇𝗇𝖾𝗅 👇
 
-👨‍💻◦ https://www.youtube.com/@malvintech2 
+👨‍💻◦ https://youtube.com/@official_manuel 
 
 📍𝖯𝗅𝖾𝖺𝗌𝖾 𝖥𝗈𝗅𝗅𝗈𝗐 𝖬𝗒 𝖶𝗁𝖺𝗍𝗌𝖺𝗉𝗉 𝖢𝗁𝖺𝗇𝗇𝖾𝗅 👇
 
-👨‍💻◦ https://whatsapp.com/channel/0029Vac8SosLY6d7CAFndv3Z
+👨‍💻◦ https://whatsapp.com/channel/0029Va8YUl50bIdtVMYnYd0E
 
 > *©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍᴀʟᴠɪɴ ᴛᴇᴄʜ*
 `
